40f0c6fe3ed7918f550c8df89de1fda94b7d6e9a
Zac McClung <zac.mcclung370@gmail.com>
2018-03-28 21:59:33 -0600

    initial commit -m

--- components/Calculator.js
+++ components/Calculator.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import Display from './Display';
-import NumbersBtn from './NumbersBtn';
-import OperationsBtn from './OperationsBtn';
 import styles from '../public/style.css';
 
 class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
   render() {
     return (
       <div className="calculator-main">
-        <Display />
-        <div className="all-btns">
-          <NumbersBtn />
-          <OperationsBtn />
-        </div>
+        <h1>attempt number 2</h1>
       </div>
     );
   }
