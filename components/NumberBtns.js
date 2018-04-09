5c6d1737708fe317c92ec6cc9b3f88f4c1c85149
Zac McClung <zac.mcclung370@gmail.com>
2018-04-08 21:26:22 -0600

    Adding buttons

--- components/NumberBtns.js
+++ components/NumberBtns.js
@@ -4,7 +4,16 @@ class NumberBtns extends React.Component {
   render() {
     return (
       <div>
-        <h1>number buttons go here!</h1>
+        <button value="0">0</button>
+        <button value="1">1</button>
+        <button value="2">2</button>
+        <button value="3">3</button>
+        <button value="4">4</button>
+        <button value="5">5</button>
+        <button value="6">6</button>
+        <button value="7">7</button>
+        <button value="8">8</button>
+        <button value="9">9</button>
       </div>
     );
   }
